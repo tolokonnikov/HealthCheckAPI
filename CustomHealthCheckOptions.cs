ac7ae81364915796f5b2d0401a657b7ec3336d41
Albert Tolokonnikov <tolokonnikov.albert@gmail.com>
2023-01-27 00:25:28 +0200

    Add health check with custom json response

---
+++ CustomHealthCheckOptions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Net.Mime;
+using System.Text.Json;
+
+namespace HealthCheckAPI
+{
+    public class CustomHealthCheckOptions : HealthCheckOptions
+    {
+        public CustomHealthCheckOptions() : base()
+        {
+            var jsonSerilazerOptions = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            ResponseWriter = async (c, r) =>
+            {
+                c.Response.ContentType = MediaTypeNames.Application.Json;
+                c.Response.StatusCode = StatusCodes.Status200OK;
+
+                var result = JsonSerializer.Serialize(new
+                {
+                    checks = r.Entries.Select(e => new
+                    {
+                        name = e.Key,
+                        responseTime = e.Value.Duration.TotalNanoseconds,
+                        status = e.Value.Status.ToString(),
+                        description = e.Value.Description,
+                    }),
+                    totalStatus = r.Status,
+                    totalResponseTime = r.TotalDuration.TotalMilliseconds,
+                }, jsonSerilazerOptions);
+
+                await c.Response.WriteAsync(result);
+            };
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+global using HealthCheckAPI;
+
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddHealthChecks()
+    .AddCheck("ICMP_01", 
+    new ICMPHealthCheck("www.ryadel.com", 100))
+    .AddCheck("ICMP_02",
+    new ICMPHealthCheck("www.google.com", 100))
+    .AddCheck("ICMP_03",
+    new ICMPHealthCheck($"www.{Guid.NewGuid():N}.com", 100));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -33,6 +43,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseHealthChecks(new PathString("/api/health"), new CustomHealthCheckOptions());
 
 app.MapControllers();
 
